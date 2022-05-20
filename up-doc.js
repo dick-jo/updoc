79fa505e54e23aa54e80c585b77c2b10f754b772
dick-jo <jones.rrw@gmail.com>
2022-05-20 15:52:28 +0930

    Create

---
+++ up-doc.js
@@ -0,0 +1,66 @@
+class UpDoc {
+  _sY = 0
+  _threshold = 0
+
+  targets = {
+    sel: `[data-updoc]`,
+    el: null,
+  }
+
+  constructor(threshold = 0) {
+    // Get DOM elements
+    this.targets.el = document.querySelectorAll(this.targets.sel)
+
+    // Set threshold (optional)
+    this.threshold = threshold
+  }
+
+  set sY(sY = window.scrollY) {
+    this._sY = sY
+  }
+
+  set threshold(threshold = 0) {
+    this._threshold = threshold
+  }
+
+  // Up/Down handling
+  down = () => {
+    this.targets.el.forEach((el) => {
+      el.dataset.updoc = 'down'
+    })
+    this.sY = window.scrollY
+  }
+
+  up = () => {
+    this.targets.el.forEach((el) => {
+      el.dataset.updoc = 'up'
+    })
+    this.sY = window.scrollY
+  }
+
+  // Events
+  onScroll = () => {
+    if (window.scrollY + this._threshold < this._sY) {
+      requestAnimationFrame(this.up)
+    } else if (window.scrollY > this._sY + this._threshold) {
+      requestAnimationFrame(this.down)
+    }
+  }
+
+  bindScroll() {
+    document.addEventListener('scroll', this.onScroll)
+  }
+
+  unbindScroll = () => {
+    document.removeEventListener('scroll', this.onScroll)
+  }
+
+  // INIT
+  init() {
+    this.bindScroll()
+
+    return this
+  }
+}
+
+export const upDoc = new UpDoc().init()
